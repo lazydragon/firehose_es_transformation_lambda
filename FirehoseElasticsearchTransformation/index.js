ac5a53e4ae31bf2c435402200f20d7467f0235c9
EC2 Default User <ec2-user@ip-172-31-6-69.ec2.internal>
2018-01-08 07:30:20 +0000

    decode base64 data and encode it back

--- FirehoseElasticsearchTransformation/index.js
+++ FirehoseElasticsearchTransformation/index.js
@@ -6,17 +6,22 @@ var geoip = require('geoip-lite');
 exports.handler = (event, context, callback) => {
     /* Process the list of records and transform them */
     const output = event.records.map(function(record){
-        var data = record.data;
+        // decode base64 json data
+        var buffer = new Buffer(record.data, 'base64');
+        var data = JSON.parse(buffer);
         // find ip information and transform it to geo ip
         var ip = data["ip"];
         var geo = geoip.lookup(ip);
         if (geo){
             data["location"] = geo.ll;
         }
+        // encode the data to base64
+        var data_str = JSON.stringify(data)
+        var result = new Buffer(data_str).toString('base64');
         return {
             recordId: record.recordId,
             result: 'Ok',
-            data: data,
+            data: result 
         }
     });
     console.log(`Processing completed.  Successful records ${output.length}.`);
