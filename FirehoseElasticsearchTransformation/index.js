1f5b23f619b5eb5cb5b5184d6f3e5ea681ee82fe
EC2 Default User <ec2-user@ip-172-31-6-69.ec2.internal>
2018-01-08 03:35:14 +0000

    add concrete function

--- FirehoseElasticsearchTransformation/index.js
+++ FirehoseElasticsearchTransformation/index.js
@@ -1,4 +1,22 @@
+'use strict';
+console.log('Loading function');
+
+var geoip = require('geoip-lite');
+
 exports.handler = (event, context, callback) => {
-    // TODO implement
-    callback();
+    /* Process the list of records and transform them */
+    const output = event.records.map(function(record){
+        var data = record.data;
+        // find ip information and transform it to geo ip
+        var ip = data["ip"];
+        var geo = geoip.lookup(ip);
+        data["location"] = geo.ll;
+        return {
+            recordId: record.recordId,
+            result: 'Ok',
+            data: data,
+        }
+    });
+    console.log(`Processing completed.  Successful records ${output.length}.`);
+    callback(null, { records: output });
 };
